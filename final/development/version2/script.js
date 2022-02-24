6f5ba5f1bb2b22ad19ac83c3508f06385c1372dd
kgovind12 <kgovind@ucdavis.edu>
2022-02-24 09:52:26 -0800

    Fixed button styles as per Glenda's feedback

--- final/development/version2/script.js
+++ final/development/version2/script.js
@@ -12,10 +12,23 @@
                 chip.classList.remove('selected');
                 numSelected--;
                 continueBtn.setAttribute('href', `plant.html?selected=${numSelected}`);
+
+                // If no options are selected, "disable" continue button
+                if (numSelected === 0) {
+                    continueBtn.style.pointerEvents = 'none';
+                    continueBtn.style.opacity = 0.7;
+                    continueBtn.style.backgroundColor = 'white';
+                    continueBtn.style.color = '#09a24e';
+                }
+   
             } else {
                 chip.classList.add('selected');
                 numSelected++;
                 continueBtn.setAttribute('href', `plant.html?selected=${numSelected}`);
+                continueBtn.style.pointerEvents = 'auto';
+                continueBtn.style.opacity = 1;
+                continueBtn.style.backgroundColor = '#09a24e';
+                continueBtn.style.color = 'white';
             }
         });
     }
