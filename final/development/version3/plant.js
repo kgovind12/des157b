679bee10880e639755b24483fd3ef7116ccfeb44
kgovind12 <kgovind@ucdavis.edu>
2022-03-07 00:22:44 -0800

    restructured add activity UI, added share and info btns

---
+++ final/development/version3/plant.js
@@ -0,0 +1,180 @@
+(function() {
+    'use strict';
+
+    Parse.serverURL = 'https://parseapi.back4app.com/'
+    Parse.initialize(
+        'cMQQ5iQeoQu0sB55PNk8u1Ipv79Ubvi8rsLq06lr', // This is your Application ID
+        'YQeJdbmuCxsZfU4dazDPENrdFhwLg4eUVFqj7tY5' // This is your Javascript key
+    );
+
+    let numFrames = localStorage.getItem('plantHeight');
+    const plantSrc = localStorage.getItem('plantSrc');
+    const root = document.querySelector(':root');
+    const communityGardenBtn = document.getElementById('communitygarden');
+    const addActivityBtn = document.getElementById('addnew');
+    const infoBtn = document.querySelector('.info-btn');
+    const shareBtn = document.querySelector('.share-btn');
+
+    // Overlay form to submit new activity
+    const addActivityOverlay = document.querySelector('#addActivity-overlay');
+    const overlayBg = document.querySelector('.overlay-bg');
+    const chips = document.querySelectorAll('.chip');
+    const detailForm = document.getElementById('addActivity-detail');
+    const submitBtn = document.getElementById('submit');
+    const addActivityCloseBtn = document.querySelector('#addActivity-close');
+
+    // Overlay form to show information
+    const infoOverlay = document.querySelector('#info-overlay');
+    const infoCloseBtn = document.querySelector('#info-close');
+    
+    // Overlay to share progress on community garden
+    const shareOverlay = document.querySelector('#share-overlay');
+    const shareCloseBtn = document.querySelector('#share-close');
+    const noBtn = document.querySelector('#no');
+    const yesBtn = document.querySelector('#yes');
+
+
+    let spritesheetSrc = '';
+    if (plantSrc === 'images/plant1.png') {
+        spritesheetSrc = 'images/spritesheet.png';
+    } else if (plantSrc === 'images/plant2.png') {
+        spritesheetSrc = 'images/spritesheet2.png';
+    } else {
+        console.log('No valid plant image selected');
+    }
+
+    console.log(numFrames); // height of plant
+    let numSelected = 0;
+
+    // Set the plant height to max
+    if (numFrames >= 7) {
+        numFrames = 7;
+        console.log("You completed all the activities for today!");
+    }
+
+    root.style.setProperty('--plant-height', numFrames);
+    root.style.setProperty('--plant-type', `url("${spritesheetSrc}")`);
+
+    // Open the add activity overlay
+    addActivityBtn.addEventListener('click', function() {
+        addActivityOverlay.classList.remove('hidden');
+        overlayBg.classList.remove('hidden');
+    });
+
+    // Open info overlay
+    infoBtn.addEventListener('click', function() {
+        infoOverlay.classList.remove('hidden');
+        overlayBg.classList.remove('hidden');
+    });
+
+    // Open the share overlay
+    shareBtn.addEventListener('click', function() {
+        shareOverlay.classList.remove('hidden');
+        overlayBg.classList.remove('hidden');
+    });
+
+    // Close add activity overlay
+    addActivityCloseBtn.addEventListener('click', function() {
+        addActivityOverlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+    });
+
+    // Close info overlay
+    infoCloseBtn.addEventListener('click', function() {
+        infoOverlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+    });
+
+    // Close the share overlay
+    shareCloseBtn.addEventListener('click', function() {
+        shareOverlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+    });
+
+    // 'No' button also closes overlay
+    noBtn.addEventListener('click', function() {
+        shareOverlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+    });
+
+    yesBtn.addEventListener('click', function() {
+        // Save the plant to the database
+        // Attributes: 1. Plant img src 2. Height 3. Description?
+        savePlantToDB();
+        shareOverlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+    });
+
+    // Find the number of entries in the database for any particular user
+
+    // TODO: Get this code working, hook it up to another page called community garden
+    // Pull all the database entries and display them there. 
+    // First start with list or grid format, then expand from there if time permits.
+    communityGardenBtn.addEventListener('click', function() {
+        console.log('Community Garden button clicked');
+    });
+
+    // TODO: Add another spritesheet. Allow users to choose between multiple plants.
+
+    // Handling chip selection
+    for (let chip of chips) {
+        chip.addEventListener('click', function() {
+            if (chip.classList.contains('selected')) {
+                chip.classList.remove('selected');
+                numSelected--;
+
+                // If no options are selected, "disable" continue button
+                if (numSelected === 0) {
+                    submitBtn.classList.add('disabled');
+                }
+   
+            } else {
+                chip.classList.add('selected');
+                numSelected++;
+                submitBtn.classList.remove('disabled');
+            }
+        });
+    }
+
+    // Handle submit of add activity form
+    submitBtn.addEventListener('click', function() {
+        // Hide the overlay
+        overlay.classList.add('hidden');
+        overlayBg.classList.add('hidden');
+
+        console.log("You selected ", numSelected);
+        localStorage.setItem('plantHeight', numSelected + parseInt(localStorage.getItem('plantHeight')));
+        // localStorage.setItem('plantHeight', 0);
+
+        let plantHeight = localStorage.getItem('plantHeight');
+
+        if (plantHeight >= 7) {
+            plantHeight = 7;
+            console.log("All activiites completed!");
+        }
+
+        root.style.setProperty('--plant-height', plantHeight);
+
+        // Unselect all the chips
+        for (let chip of chips) {
+            chip.classList.remove('selected');
+        }
+        numSelected = 0;
+    });
+
+    async function savePlantToDB() {
+        const plantData = new Parse.Object('Plants');
+        let plantHeight = parseInt(localStorage.getItem('plantHeight'));
+
+        console.log("Saving plant data to db");
+
+        plantData.set('plantSrc', spritesheetSrc);
+        plantData.set('plantHeight', plantHeight);
+
+        try {
+            const result = await plantData.save();
+        } catch (error) {
+            console.log('Could not add activity: ', error);
+        }
+    }
+})();
---
+++ final/development/version3/script.js
@@ -0,0 +1,125 @@
+(function() {
+    'use strict';
+
+    Parse.serverURL = 'https://parseapi.back4app.com/'
+    Parse.initialize(
+        'cMQQ5iQeoQu0sB55PNk8u1Ipv79Ubvi8rsLq06lr', // This is your Application ID
+        'YQeJdbmuCxsZfU4dazDPENrdFhwLg4eUVFqj7tY5' // This is your Javascript key
+    );
+
+    const chips = document.querySelectorAll('.chip');
+    const continueBtn = document.querySelector('#continue');
+    const toPage2Btn = document.querySelector('#toPage2');
+    const toPage3Btn = document.querySelector('#toPage3');
+
+    // Form pages
+    const formPage1 = document.querySelector('.page1');
+    const formPage2 = document.querySelector('.page2');
+    const formPage3 = document.querySelector('.page3');
+
+    // Form input
+    const detailForm = document.querySelector('#detail');
+
+    // Plants
+    const plantImages = document.querySelectorAll('.plant-img');
+
+    const rectangleBg = document.querySelector('.rectangle-bg');
+    const backBtn = document.querySelector('.back-btn');
+
+    let numSelected = 0;
+
+    // Handle click of each chip
+    for (let chip of chips) {
+        chip.addEventListener('click', function() {
+            if (chip.classList.contains('selected')) {
+                chip.classList.remove('selected');
+                numSelected--;
+
+                // If no options are selected, "disable" continue button
+                if (numSelected === 0) {
+                    toPage2Btn.classList.add('disabled');
+                }
+   
+            } else {
+                chip.classList.add('selected');
+                numSelected++;
+                toPage2Btn.classList.remove('disabled');
+            }
+        });
+    }
+
+    // Handle button click to continue to page 2
+    toPage2Btn.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        // Show the back button. Handle slide in of second form
+        backBtn.classList.remove('hidden');
+        formPage1.classList.add('slideout');
+        formPage2.classList.add('slidein');
+        rectangleBg.classList.add('slide');
+
+        // Plant height is the number of activities selected
+        let plantHeight = document.querySelectorAll('.selected').length;
+
+        // Save this plantHeight to local storage
+        localStorage.setItem('plantHeight', plantHeight + parseInt(localStorage.getItem('plantHeight')));
+
+        // The following line is used for resetting the plant height for testing purposes
+        // localStorage.setItem('plantHeight', 0);
+
+        rectangleBg.addEventListener('animationend', function() {
+            console.log("this transition ended");
+            this.classList.remove('slide');
+        });
+    });
+
+    // Handle button click for continuing to page 3
+    toPage3Btn.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        formPage2.classList.add('slideout');
+        formPage2.classList.remove('slidein');
+        formPage3.classList.add('slidein');
+        rectangleBg.classList.add('slide');
+
+        rectangleBg.addEventListener('animationend', function() {
+            this.classList.remove('slide');
+        });
+    });
+
+    // Handle back button press
+    backBtn.addEventListener('click', function() {
+        this.classList.add('hidden');
+        formPage1.classList.remove('slideout');
+        formPage2.classList.remove('slidein');
+        rectangleBg.classList.add('slide');
+    });
+
+    // Handle button for continuing to the plant page
+    continueBtn.addEventListener('click', function(event) {
+        if (numSelected === 0) {
+            event.preventDefault();
+            alert('Please select at least one activity.');
+            return;
+        }
+        location.href = 'plant.html';
+    });
+
+    // Click handler for each plant image (to select a type of plant)
+    plantImages.forEach(function(plantImg) {
+        plantImg.addEventListener('click', function() {
+            let plantSrc = this.getAttribute('src');
+            localStorage.setItem('plantSrc', plantSrc);
+
+            for (let image of plantImages) {
+                image.classList.remove('green');
+            }
+
+            if (this.classList.contains('green')) {
+                this.classList.remove('green');
+            } else {
+                this.classList.add('green');
+            }
+        });
+    });
+})();
