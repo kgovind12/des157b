2549cafe59bd8fe6e0dd684cabd2437eac85160c
kgovind12 <kgovind@ucdavis.edu>
2022-03-07 00:59:38 -0800

    Added community garden page. Very basic UI

---
+++ final/development/version3/communitygarden.js
@@ -0,0 +1,7 @@
+(function() {
+    'use strict';
+
+    // TODO: Get the first three plants from db
+    // set root of plant1-height, plant2-height, and plant3-height;
+    // Repeat the same for plant-types.
+})();
