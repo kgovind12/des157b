080a68bf4b11a51dc3bfbcb2dcae955351dc3f10
kgovind12 <kgovind@ucdavis.edu>
2022-05-05 02:08:49 -0700

    Style and content changes

--- studio2/script.js
+++ studio2/script.js
@@ -12,11 +12,13 @@
 
             // Add all the bubbles as html to the circle
             circle.innerHTML += `<div class="bubble hidden" id="bubble-${point}">
+                <div class="flex-row">
+                    <h2>${jsonData[point].month}</h2>
+                    <p>Feeling</p>
+                    <h3> ${jsonData[point].feeling}</h3>
+                </div>
+                
                 <img src="images/${jsonData[point].feeling.toLowerCase()}.png" alt="${jsonData[point].feeling} bitmoji">
-                <h2>${jsonData[point].month}</h2>
-                <p>Feeling</p>
-                <h3> ${jsonData[point].feeling}</h3>
-                <p>${jsonData[point].reason}</p>
             </div>`;
         });
     }
